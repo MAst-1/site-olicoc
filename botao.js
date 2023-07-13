86ba7e73677dfa52bbde5da6b19844014aa1de2b
Matheus <livematgreg@gmail.com>
2023-07-13 19:11:18 -0300

    new file:   botao.js 	new file:   bsqt.html 	new file:   fut.html 	new file:   hnd.html 	new file:   imagens/baquete-imagem2.svg.svg 	new file:   imagens/basquete.svg.svg 	new file:   imagens/futebol.svg 	new file:   imagens/imagem-aqui.svg 	new file:   index.html 	new file:   o.html 	new file:   style.css 	new file:   v.html

---
+++ botao.js
@@ -0,0 +1,13 @@
+const btnMenu = document.getElementById('btn-menu');
+function toggleMenu(event) {
+    if(event.type=== 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+btnMenu.addEventListener('click', toggleMenu );
+btnMenu.addEventListener('touchstart', toggleMenu );
+
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('#header')
+    header.classList.toggle('rolagem',window.scrollY>0)
+})
