9789b31faf763a3bbdd90fe416af6fef1e42c5e6
Matheus <livematgreg@gmail.com>
2023-07-16 22:07:26 -0300

    modified:   bsqt.html     modified:   fut.html 	modified:   hnd.html 	modified:   index.html 	modified:   o.html 	renamed:    botao.js -> script/script.js 	modified:   v.html

--- script/script.js
+++ script/script.js
@@ -1,11 +1,10 @@
 const btnMenu = document.getElementById('btn-menu');
-const btnVoltar = document.getElementsByClassName('container-s');
-
 function toggleMenu(event) {
     if(event.type=== 'touchstart') event.preventDefault();
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
 }
+
 btnMenu.addEventListener('click', toggleMenu );
 btnMenu.addEventListener('touchstart', toggleMenu );
 
